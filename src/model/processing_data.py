2212bf880a6ae0332494f44a45a7b8cf2748f71d
Susana Xu <yimingxu@g.harvard.edu>
2019-12-01 23:36:26 -0500

    fix bugs in sampling

--- src/model/processing_data.py
+++ src/model/processing_data.py
@@ -48,7 +48,7 @@ def day_reward(telescope_name, day_current_str, end_day_str, start_time, end_tim
 
     if sampled:
         sample_df = pd.DataFrame(
-            np.random.normal(df_tau_all['latest'], df_tau_all['mse'], (100, len(df_tau_all))).T)  # todo
+            np.random.normal(df_tau_all['latest'], df_tau_all['mse'], (100, len(df_tau_all))).T)
         df_tau_all = pd.concat([df_tau_all[['date', 'day']], sample_df], axis=1)
         df_tau_day = pd.DataFrame(df_tau_all.groupby('day').mean())
 
@@ -76,7 +76,7 @@ def all_day_reward(day_current_str, end_day_str, time_std=False, further_std=Fal
     based on area_i/total_area
     """
 
-    if distance:
+    if distance and not sampled:
         # set up a dataframe
         telescopes_day_reward = day_reward(settings.telescopes[0], day_current_str, end_day_str, \
                                            settings.dict_schedule[settings.telescopes[0]][0],
@@ -95,7 +95,6 @@ def all_day_reward(day_current_str, end_day_str, time_std=False, further_std=Fal
                                                 sampled=sampled) \
                                      * settings.dict_weight[i]
             f_T = np.hstack([f_T, new_telescope.values])
-
         # calculate the number of telescopes
         num_tele = len(settings.telescopes)
 
@@ -103,11 +102,59 @@ def all_day_reward(day_current_str, end_day_str, time_std=False, further_std=Fal
         # f.T is d * n, d is number of days, n is number of telescopes
         # D is n * n, element-wise reciprocal of baseline length matrix
         # 1 is n * 1, a column vector of 1's
-        F = f_T @ np.reciprocal(settings.baseline_lengths) @ np.array([[1]*num_tele]).T
+        inv_D = settings.baseline_lengths * 1.0
+        inv_D[inv_D != 0] = 1 / inv_D[inv_D != 0]
+        F = f_T @ inv_D @ np.array([[1]*num_tele]).T
 
         # replace values in the dataframe with indices as days
         telescopes_day_reward['value'] = F
         F = telescopes_day_reward / sum(settings.weights)
+
+    if distance and sampled:
+        # for the sampling method
+        # the returned dataframe will be in shape d * 100, d is number of days, 100 is the number of samples we take
+
+        # run for the first telescope to get number of days
+        new_telescope = day_reward(settings.telescopes[0], day_current_str, end_day_str, \
+                                           settings.dict_schedule[settings.telescopes[0]][0],
+                                           settings.dict_schedule[settings.telescopes[0]][1], \
+                                           time_std=time_std, further_std=further_std, punish_level=punish_level,
+                                           sampled=sampled) \
+                                * settings.dict_weight[settings.telescopes[0]]
+
+        # calculate the number of telescopes
+        num_tele = len(settings.telescopes)
+
+        # record sampled values
+        sampled_f = np.zeros((num_tele, new_telescope.values.shape[0], new_telescope.values.shape[1]))
+        index = 0
+        sampled_f[index] = new_telescope.values
+
+
+        for i in settings.telescopes[1:]:
+            index += 1
+            new_telescope = day_reward(i, day_current_str, end_day_str, \
+                       settings.dict_schedule[i][0], settings.dict_schedule[i][1], \
+                       time_std=time_std, further_std=further_std, punish_level=punish_level,
+                       sampled=sampled) * settings.dict_weight[i]
+            sampled_f[index] = new_telescope.values
+
+
+
+        # slicing each set of sample
+        for j in range(100): #because we sampled 100 tao-225 for each telescope on each day
+            # calculate f.T @ D @ 1
+            # f.T is d * n, d is number of days, n is number of telescopes
+            # D is n * n, element-wise reciprocal of baseline length matrix
+            # 1 is n * 1, a column vector of 1's
+            f_T = np.hstack([sampled_f[k, :, j].reshape(-1,1) for k in range(num_tele)])
+            inv_D = settings.baseline_lengths * 1.0
+            inv_D[inv_D != 0] = 1 / inv_D[inv_D != 0]
+            F = f_T @ inv_D @ np.array([[1] * num_tele]).T
+            # replace values in the dataframe
+            new_telescope.loc[:, j] = F
+        F = new_telescope / sum(settings.weights)
+
     else:
         # set up a dataframe
         telescopes_day_reward = day_reward(settings.telescopes[0], day_current_str, end_day_str, \
@@ -115,6 +162,7 @@ def all_day_reward(day_current_str, end_day_str, time_std=False, further_std=Fal
                                            time_std=time_std, further_std=further_std, punish_level=punish_level,
                                            sampled=sampled) \
                                 * settings.dict_weight[settings.telescopes[0]]
+
         # fill up the dataframe
         for i in settings.telescopes[1:]:
             telescopes_day_reward += day_reward(i, day_current_str, end_day_str, \
@@ -123,11 +171,7 @@ def all_day_reward(day_current_str, end_day_str, time_std=False, further_std=Fal
                                                 sampled=sampled) \
                                      * settings.dict_weight[i]
         F = telescopes_day_reward / sum(settings.weights)
+
     return F
 
-
-#
-whole_time_window = all_day_reward(read_data.start_date, read_data.end_date, distance = True)
-# days = np.array(whole_time_window.index)
-print(whole_time_window)
 print("processing_data")
